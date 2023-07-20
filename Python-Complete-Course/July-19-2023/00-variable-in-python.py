20634d2080e3d6119875967cf1b5a85395be8723
Abhishek Kumar <Abhishek.Kumar@watchguard.com>
2023-07-20 10:17:55 +0530

    task 19 July

---
+++ Python-Complete-Course/July-19-2023/00-variable-in-python.py
@@ -0,0 +1,21 @@
+age = 30
+print(age)
+
+age = 40
+print(age)
+
+# we use snake case in python
+friend_age = 25
+country_visited = 5
+
+PI = 3.14
+
+# float Division
+print(10 / 3 )  # 3.3333333
+
+# Integer Division
+print(10 // 3 ) # 3
+
+# Remainder
+remainder = 10 % 3
+print("remainder : ",remainder)
