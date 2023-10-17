8c35586417a0cb5655e79c4346dc0eb609a160f7
abhishek300830 <abhishek300830@gmail.com>
2023-10-17 22:03:14 +0530

    basic javascript

---
+++ Complete Javascript Course WG/01-writing-basics/script.js
@@ -0,0 +1,4 @@
+document.write("this is written by script.js")
+setTimeout(function() {alert("This is written by script.js")},3000)
+console.log('hello world')
+console.log("%ccolored hello world","color:white;background:black")
---
+++ Complete Javascript Course WG/02-Prompts/script.js
@@ -0,0 +1,6 @@
+
+question = "what is your name?"
+
+username = prompt(question)
+
+alert("Hello " + username + "!")
