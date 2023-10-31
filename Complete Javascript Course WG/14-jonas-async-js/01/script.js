fa8aec04b54aeb05c5f940d500fa81f817e2317c
abhishek300830 <abhishek300830@gmail.com>
2023-10-31 17:38:44 +0530

    working on reaction game

---
+++ Complete Javascript Course WG/14-jonas-async-js/01/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const request = new XMLHttpRequest()
+request.open('GET',"https://restcountries.com/v3.1/name/India");
+
+request.send()
+
+request.addEventListener('load',function(){
+    const data = JSON.parse(this.responseText)
+    console.log(data)
+})
